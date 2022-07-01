3058737847d5ff930d182c5ecfacadb168280910
chepugash <chepugash@yahoo.com>
2022-07-01 22:58:33 +0300

    new commit

--- SP22/app/src/main/java/com/example/sp22/MusicFragment.kt
+++ SP22/app/src/main/java/com/example/sp22/MusicFragment.kt
@@ -1,6 +1,25 @@
 package com.example.sp22
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 
 class MusicFragment : Fragment(R.layout.fragment_music) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val text = arguments?.getString(ARG_TEXT).orEmpty()
+        if (text.isNotEmpty()) {
+            Snackbar.make(view, text, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        private const val ARG_TEXT = "amazing"
+        fun createBundle(text: String): Bundle {
+            val bundle = Bundle()
+            bundle.putString(ARG_TEXT, text)
+            return bundle
+        }
+    }
 }
--- SP22/app/src/main/java/com/example/sp22/ProfileFragment.kt
+++ SP22/app/src/main/java/com/example/sp22/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.sp22
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.sp22.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
@@ -12,6 +13,13 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentProfileBinding.bind(view)
+
+        with (binding) {
+            btnMusic.setOnClickListener {
+                findNavController().navigate(R.id.action_profileFragment_to_musicFragment,
+                    MusicFragment.createBundle((etText.text ?: "Error").toString()))
+            }
+        }
     }
 
     override fun onDestroy() {
