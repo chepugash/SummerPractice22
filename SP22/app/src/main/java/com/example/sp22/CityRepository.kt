728a44d6a51d5c8bdbc4cc6b3c23aec5ba857ed1
chepugash <chepugash@yahoo.com>
2022-07-02 23:24:57 +0300

    DetailsFragment added

--- SP22/app/src/main/java/com/example/sp22/CityRepository.kt
+++ SP22/app/src/main/java/com/example/sp22/CityRepository.kt
@@ -9,14 +9,14 @@ object CityRepository {
         City(2, "Tashkent", "Uzbekistan",
             "https://cdnn21.img.ria.ru/images/156098/88/1560988826_0:207:2732:1744_1920x0_80_0_0_294c9d8d8bad959093ffcb2809791984.jpg"),
         City(3, "Paris", "France",
-            "http://travelukr.com.ua/wp-content/uploads/2014/07/Paris2.jpg"),
+            "https://www.pandotrip.com/wp-content/uploads/2013/08/1D110-Bertrand-Monney.jpg"),
         City(4, "Washington", "USA",
             "https://www.iamconsortium.org/wp-content/uploads/2020/03/Third-Annual-Meeting-of-the-IAMC.jpg"),
         City(5, "Berlin", "Germany",
-            "https://aws-tiqets-cdn.imgix.net/images/content/cae4bc5ad63244419cc13734d2bd6dc6.jpg?auto=format&fit=crop&ixlib=python-3.2.1&q=70&s=13f7158ba7b8bd4cd582736742eb5416"),
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwJaB-WUKScZajeAZIwxxxuEw1C62XBJO9gg&usqp=CAU"),
         City(6, "London", "England",
             "https://cdnn21.img.ria.ru/images/155635/45/1556354505_0:438:2000:1563_1920x0_80_0_0_64fb6d242c6240d9e44e5de1a31a7d6f.jpg"),
         City(7, "Beijing", "China",
-            "https://wallpaperaccess.com/full/134995.jpg"),
+            "https://quicksilvertranslate.com/wp-content/uploads/iu-10-3-710x400.jpeg"),
     )
 }
---
+++ SP22/app/src/main/java/com/example/sp22/DetailsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.sp22
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
